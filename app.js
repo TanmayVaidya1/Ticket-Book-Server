da6563cb756f03b64adb46a0e7bff684d7d01e2c
Tanmay <tanmayvaidya742@gmail.com>
2024-08-03 11:31:17 +0530

    Third Commit

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const app = express();
 
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const PORT = 8000;
+// const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 const cookieParser = require('cookie-parser');
 
 const authRoutes = require('./Routes/Auth');
